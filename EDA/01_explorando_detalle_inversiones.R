4208129d953231a6d9b7907aa812612cc404bbb5
Sefran <10433628+sefran12@users.noreply.github.com>
2022-06-21 01:35:47 +0200

    "Initial EDA"

---
+++ EDA/01_explorando_detalle_inversiones.R
@@ -0,0 +1,58 @@
+# Initial exploration of dataset
+library(tidyverse)
+library(broom)
+library(skimr)
+library(lubridate)
+library(ggpmisc)
+
+options(scipen = 99999, digits = 9)
+
+# Load data
+detalle_inversiones <- read.csv(file = "data/01_raw/DETALLE_INVERSIONES.csv")
+
+# skim
+skim(detalle_inversiones)
+
+#
+with(detalle_inversiones, plot(log(MONTO_VIABLE + 1) ~ log(COSTO_ACTUALIZADO + 1), pch = '.'))
+
+# hay montos tras los cuales se requieren mayores estudios. Hay conglomeración justo antes?
+# hasta 1 200 000: Perfil simplificado
+# hasta 10 000 000: Perfil
+# Mas de 10 000 000: Factibilidad
+
+detalle_inversiones %>% 
+    filter(MONTO_VIABLE < 20000000,
+           MONTO_VIABLE > 100000) %>% 
+    ggplot(aes(x = MONTO_VIABLE)) +
+    geom_histogram(bins = 250) +
+    geom_vline(xintercept = c(1200000, 10000000), color = "red") +
+    scale_x_log10() 
+
+detalle_inversiones %>%  
+    filter(MONTO_VALORIZACION < 20000000,
+           MONTO_VALORIZACION > 100000) %>% 
+    ggplot(aes(x = MONTO_VALORIZACION)) +
+    geom_histogram(bins = 250) +
+    geom_vline(xintercept = c(1200000, 10000000), color = "red") +
+    scale_x_log10() 
+
+detalle_inversiones %>%  
+    select(MONTO_VIABLE, MONTO_VALORIZACION) %>% 
+    pivot_longer(everything()) %>%
+    filter(value < 20000000,
+           value > 100000) %>% 
+    ggplot(aes(x = value, fill = name)) +
+    geom_histogram(aes(y = 0.5*..density..),
+                   bins = 350, position = "identity",
+                   alpha = 0.6) +
+    geom_vline(xintercept = c(1210000, 10010000), color = "red", lwd = 0.5) +
+    scale_x_log10() +
+    labs(x = "Monto en soles",
+         y = "Densidad",
+         color = "Monto") +
+    theme_minimal()
+
+
+table(cut(detalle_inversiones$MONTO_VIABLE, c(0, 1150000, 1200000, 1250000, 9900000, 10000000, 10100000, Inf))) %>% as.data.frame()
+
---
+++ config/data_engineering_parameters.R
@@ -0,0 +1,10 @@
+### DATA ENGINEERING PARAMETERS
+
+# This means these parts of the pipeline:
+
+# 01_data_generation
+# 02_data_preparation
+
+data_engineering_parameters = list(
+    
+)
