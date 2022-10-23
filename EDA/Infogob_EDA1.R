0a5887cb3d8e9df1c5ff384d8279e65c0e269252
kurrutia-turing <kilder.urrutia@turing.com>
2022-10-23 16:12:02 +0200

    updated infogob parser to include Distritos and Provincias from 2002 to 2018. this is both results and politicians parsers

--- EDA/Infogob_EDA1.R
+++ EDA/Infogob_EDA1.R
@@ -92,3 +92,69 @@ full_data %>%
 full_data %>% 
     count(organizacion_politica, region) %>% 
     arrange(desc(n))
+
+
+### FULL DATA
+
+file_list <- dir(path = "data/01_raw/Base_de_datos_INFOgob/", full.names = TRUE, recursive = TRUE)
+
+full_data = data.frame()
+for (curr_file in file_list) {
+    cat("Processing", curr_file, "\n")
+    
+    if (!grepl(pattern = "Resultados", curr_file) | !grepl(pattern = "Provincial|Distrital", curr_file)) {
+        cat("Not a results file, skipping...", "\n")
+        next
+    }
+    
+    # read data
+    curr_data = read_excel(curr_file)
+    curr_data = janitor::clean_names(curr_data)
+    
+    # basic feature transformation
+    curr_data$year = str_extract(curr_file, "[1-2][0-9]{3}")
+    curr_data$tipo_municipalidad = ifelse(grepl("PROVINCIAL", curr_file), "provincial", "distrital")
+    if (!("distrito" %in% colnames(curr_data))) {
+        curr_data$distrito = "general"
+    }
+#    curr_data$vuelta = ifelse(grepl("2V", curr_file), "2", "1")
+    
+    full_data <- full_data %>% rbind(curr_data)
+}
+
+
+## AUTORIDADES PROVINCIALES Y DISTRITALES
+
+full_data = data.frame()
+for (curr_file in file_list) {
+    cat("Processing", curr_file, "\n")
+    
+    if (!grepl(pattern = "Autoridades", curr_file) | !grepl(pattern = "Provincial|Distrital", curr_file)) {
+        cat("Not an elected officials file, skipping...", "\n")
+        next
+    }
+    
+    # read data
+    curr_data = read_excel(curr_file)
+    curr_data = janitor::clean_names(curr_data)
+    if ((length(colnames(full_data)) > 0) && any(colnames(full_data) != colnames(curr_data))) {
+        colnames(curr_data) = colnames(full_data)
+    }
+    
+    # basic feature transformation
+    curr_data$year = str_extract(curr_file, "[1-2][0-9]{3}")
+    curr_data$vuelta = ifelse(grepl("2V", curr_file), "2", "1")
+    curr_data$full_name = str_c(curr_data$prenombres, curr_data$primer_apellido, curr_data$segundo_apellido, sep = " ")
+    curr_data$tipo_municipalidad = ifelse(grepl("PROVINCIAL", curr_file), "provincial", "distrital")
+    
+    if (!("percent_votos_obtenidos_por_la_organizacion_politica" %in% colnames(curr_data))) {
+        curr_data$percent_votos_obtenidos_por_la_organizacion_politica = NA
+    }
+    
+    full_data <- full_data %>% rbind(curr_data)
+}
+
+# save
+write.csv(full_data, file = "data/02_intermediate/autoridades_electas_regionales_2010_2018.csv")
+colnames(full_data)
+colnames(curr_data)
--- src/01_data_generation/infogob.R
+++ src/01_data_generation/infogob.R
@@ -32,6 +32,7 @@ for (curr_file in file_list) {
 # save
 write.csv(full_data, file = "data/02_intermediate/resultados_electorales_regionales_2010_2018.csv")
 
+
 ## AUTORIDADES REGIONALES
 
 full_data = data.frame()
@@ -60,3 +61,67 @@ for (curr_file in file_list) {
 
 # save
 write.csv(full_data, file = "data/02_intermediate/autoridades_electas_regionales_2010_2018.csv")
+
+### RESULTADOS ELECTORALES DISTRITALES Y PROVINCIALES
+
+file_list <- dir(path = "data/01_raw/Base_de_datos_INFOgob/", full.names = TRUE, recursive = TRUE)
+
+full_data = data.frame()
+for (curr_file in file_list) {
+    cat("Processing", curr_file, "\n")
+    
+    if (!grepl(pattern = "Resultados", curr_file) | !grepl(pattern = "Provincial|Distrital", curr_file)) {
+        cat("Not a results file, skipping...", "\n")
+        next
+    }
+    
+    # read data
+    curr_data = read_excel(curr_file)
+    curr_data = janitor::clean_names(curr_data)
+    
+    # basic feature transformation
+    curr_data$year = str_extract(curr_file, "[1-2][0-9]{3}")
+    curr_data$tipo_municipalidad = ifelse(grepl("PROVINCIAL", curr_file), "provincial", "distrital")
+    if (!("distrito" %in% colnames(curr_data))) {
+        curr_data$distrito = "general"
+    }
+    #    curr_data$vuelta = ifelse(grepl("2V", curr_file), "2", "1")
+    
+    full_data <- full_data %>% rbind(curr_data)
+}
+
+write.csv(full_data, file = "data/02_intermediate/resultados_electorales_provinciales_distritales_2002_2018.csv")
+
+
+## AUTORIDADES ELEGIDAS DISTRITALES Y REGIONALES
+
+full_data = data.frame()
+for (curr_file in file_list) {
+    cat("Processing", curr_file, "\n")
+    
+    if (!grepl(pattern = "Autoridades", curr_file) | !grepl(pattern = "Provincial|Distrital", curr_file)) {
+        cat("Not an elected officials file, skipping...", "\n")
+        next
+    }
+    
+    # read data
+    curr_data = read_excel(curr_file)
+    curr_data = janitor::clean_names(curr_data)
+    if ((length(colnames(full_data)) > 0) && any(colnames(full_data) != colnames(curr_data))) {
+        colnames(curr_data) = colnames(full_data)
+    }
+    
+    # basic feature transformation
+    curr_data$year = str_extract(curr_file, "[1-2][0-9]{3}")
+    curr_data$vuelta = ifelse(grepl("2V", curr_file), "2", "1")
+    curr_data$full_name = str_c(curr_data$prenombres, curr_data$primer_apellido, curr_data$segundo_apellido, sep = " ")
+    curr_data$tipo_municipalidad = ifelse(grepl("PROVINCIAL", curr_file), "provincial", "distrital")
+    
+    if (!("percent_votos_obtenidos_por_la_organizacion_politica" %in% colnames(curr_data))) {
+        curr_data$percent_votos_obtenidos_por_la_organizacion_politica = NA
+    }
+    
+    full_data <- full_data %>% rbind(curr_data)
+}
+
+write.csv(full_data, file = "data/02_intermediate/autoridades_electas_provinciales_distritales_2002_2018.csv")
